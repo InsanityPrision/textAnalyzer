081b60402ab8414568952af8c0a5248b755fc601
Erik Riquelme <erikone2311@gmail.com>
2024-10-02 15:27:34 +0200

    Rename paragraphs to paragraphsNumber

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -1,10 +1,10 @@
 export const getNumberParagraphs = (text: string): number => {
-  const paragraphs = text.split("\n");
+  const paragraphsNumber = text.split("\n");
   if (text === "") {
     return 0;
   }
-  if (paragraphs.length === 1) {
+  if (paragraphsNumber.length === 1) {
     return 1;
   }
-  return paragraphs.length;
+  return paragraphsNumber.length;
 };

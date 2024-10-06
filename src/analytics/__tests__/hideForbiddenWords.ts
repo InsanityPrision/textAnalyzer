f99c3442805f309da514b31a9d56526b38fa14ec
Erik Riquelme <erikone2311@gmail.com>
2024-10-06 22:46:28 +0200

    Rename wordList to words

--- src/analytics/__tests__/hideForbiddenWords.ts
+++ src/analytics/__tests__/hideForbiddenWords.ts
@@ -1,9 +1,9 @@
 import { hideForbiddenWords } from "../index.js";
 
 const text = `Esto es un buen de plato`;
-const wordList: string[] = ["mierda", "juego"];
+const words: string[] = ["mierda", "juego"];
 const expectedHidenWordsText = `Esto es un buen de plato`;
-const hidenWordsText = hideForbiddenWords(text, wordList);
+const hidenWordsText = hideForbiddenWords(text, words);
 
 if (expectedHidenWordsText === hidenWordsText) {
   console.log("Test passed");

7eba0d70dbde57637f9c3bc478fef40448a8c770
Erik Riquelme <erikone2311@gmail.com>
2024-10-06 22:45:16 +0200

    Rename wantedWordFrequency to wantedWord

--- src/analytics/__tests__/getWordFrequencyTotal.ts
+++ src/analytics/__tests__/getWordFrequencyTotal.ts
@@ -2,9 +2,9 @@ import { getWordFrequencyTotal } from "../index.js";
 
 const text =
   "Hola que tal soy Erik, Erik me llaman ayer me llamaron erik. Y el otro dia eRiK: o erik;";
-const wantedWordFrequency = "erik";
+const wantedWord = "erik";
 const expectedWordFrequencyTotal = 5;
-const wordFrequencyTotal = getWordFrequencyTotal(text, wantedWordFrequency);
+const wordFrequencyTotal = getWordFrequencyTotal(text, wantedWord);
 
 if (wordFrequencyTotal === expectedWordFrequencyTotal) {
   console.log("Test passed");
